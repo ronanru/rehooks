c43158ee881e8f4283a382dda8c7e5feed029304
pyr33x <pyrexrj@gmail.com>
2024-10-24 11:14:11 +0330

    feat(cli): choices to copy

--- packages/cli/src/commands/hooks.ts
+++ packages/cli/src/commands/hooks.ts
@@ -1,16 +1,12 @@
 import { handleError } from "~/utils/error";
-import { HookOptions } from "~/types";
 import { Command } from "commander";
+import clipboardy from "clipboardy";
+import type { Hook } from "~/types";
+import inquirer from "inquirer";
 import ora from "ora";
 
-const fetchHooks = async (options: HookOptions): Promise<any> => {
-  const { search, limit } = options;
-  const queryParams = new URLSearchParams();
-
-  if (search) queryParams.append("search", search);
-  if (limit) queryParams.append("limit", limit.toString());
-
-  const BASE_URL = `https://rehooks.pyr33x.ir/api/hooks?${queryParams.toString()}`;
+const fetchHooks = async (): Promise<any> => {
+  const BASE_URL = `https://rehooks.pyr33x.ir/api/hooks`;
 
   const response = await fetch(BASE_URL, {
     method: "GET",
@@ -27,25 +23,48 @@ const fetchHooks = async (options: HookOptions): Promise<any> => {
 export const hooks = new Command()
   .name("hooks")
   .description("Get hooks from Rehooks API")
-  .argument("[search]", "Search hooks by name")
-  .argument("[limit]", "Limit the number of hooks to return (default: all)")
-  .action(
-    async (firstArg: string | undefined, secondArg: string | undefined) => {
-      const spinner = ora("Fetching hooks...").start();
+  .action(async () => {
+    const spinner = ora("Fetching hooks...").start();
 
-      try {
-        const search = isNaN(Number(firstArg)) ? firstArg : undefined;
-        const limit = !isNaN(Number(firstArg))
-          ? Number(firstArg)
-          : secondArg
-            ? Number(secondArg)
-            : undefined;
+    try {
+      const hooksData = await fetchHooks();
+      spinner.stop();
 
-        const hooksData = await fetchHooks({ search, limit });
-        spinner.stop();
-        console.log(hooksData);
-      } catch (error) {
-        spinner.fail("Error fetching hooks: " + handleError(error));
+      if (hooksData.length > 0) {
+        const choices = hooksData.map((hook: Hook) => ({
+          name: `${hook.title}`,
+          value: hook.import,
+        }));
+
+        const { selectedImports } = await inquirer.prompt([
+          {
+            type: "checkbox",
+            name: "selectedImports",
+            message: "Select hooks to copy their import statements:",
+            choices,
+          },
+        ]);
+
+        if (selectedImports.length > 0) {
+          const importNames = selectedImports.map((importStatement: string) => {
+            return importStatement.match(/{(.+?) }/)?.[1];
+          });
+
+          const uniqueImportNames = Array.from(new Set(importNames)).join(", ");
+          const finalImportStatement = `import { ${uniqueImportNames} } from 'rehooks-ts'`;
+
+          clipboardy.writeSync(finalImportStatement);
+          console.log(
+            "Copied the following import to clipboard:\n",
+            finalImportStatement,
+          );
+        } else {
+          console.log("No hooks selected.");
+        }
+      } else {
+        console.log("No hooks found.");
       }
-    },
-  );
+    } catch (error) {
+      spinner.fail("Error fetching hooks: " + handleError(error));
+    }
+  });
