e869c9f8a30d7ec21b8d438a4bd1cd95a5ff9a9e
pyr33x <pyrexrj@gmail.com>
2024-10-24 11:20:22 +0330

    feat(cli): get package utility function

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
+import { getPackageInfo } from "~/utils/package";
 import { hooks } from "~/commands/hooks";
 import { Command } from "commander";
 
-function main() {
+async function main() {
+  const packageInfo = await getPackageInfo();
   const program = new Command()
     .name("rehooks")
-    .description("A CLI for Rehooks.")
-    .version("0.0.2", "-v, --version", "display the current version");
+    .description("A CLI to copy the import state of hooks.")
+    .version(
+      packageInfo.version || "1.0.1",
+      "-v, --version",
+      "display the version number",
+    );
   program.addCommand(hooks);
   program.parse();
 }
