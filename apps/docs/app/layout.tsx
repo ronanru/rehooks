2d9630baa650adc75272e1999093bdde6899e1fc
pyr33x <pyrexrj@gmail.com>
2024-11-29 10:42:24 +0330

    feat(docs): fallback fonts

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -12,12 +12,36 @@ export const metadata = {
 } satisfies Metadata;
 
 const jetbrains = JetBrains_Mono({
+  fallback: [
+    "-apple-system",
+    "monospace",
+    "Consolas",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+  ],
+  adjustFontFallback: true,
   subsets: ["latin"],
+  preload: true,
   variable: "--font-mono",
 });
 
 const inter = Inter({
+  fallback: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+  ],
+  adjustFontFallback: true,
   subsets: ["latin"],
+  preload: true,
   variable: "--font-sans",
 });
 
