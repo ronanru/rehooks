1503b0f43eae09e45a67ef5cca808d4be0bf25f9
pyr33x <pyrexrj@gmail.com>
2024-11-29 10:49:52 +0330

    ref(docs): update cards

--- apps/docs/components/internal/card.tsx
+++ apps/docs/components/internal/card.tsx
@@ -19,65 +19,44 @@ interface Card {
   class?: string;
 }
 
-type Hook<ID extends number, T> = {
-  id: ID;
-  title: T;
-  class?: T;
+type Hook = {
+  title: string;
+  class?: string;
+  group: "A" | "B";
 };
 
-const hooks: Hook<number, string>[] = [
-  {
-    id: 1,
-    title: "useSessionStorage",
-  },
-  {
-    id: 2,
-    title: "useDebounceValue",
-  },
-  {
-    id: 3,
-    title: "useLocalStorage",
-  },
-  {
-    id: 4,
-    title: "useClipboard",
-  },
-  {
-    id: 5,
-    title: "useKeyPress",
-  },
-  {
-    id: 6,
-    title: "useScroll",
-  },
-];
+const hooks: Hook[] = [
+  { title: "useSessionStorage", group: "A" },
+  { title: "useDebounceValue", group: "A" },
+  { title: "useLocalStorage", group: "A" },
+  { title: "useClipboard", group: "A" },
+  { title: "useKeyPress", group: "A" },
+  { title: "useScroll", group: "A" },
+  { title: "useDebounceCallback", group: "B" },
+  { title: "useEventListener", group: "B" },
+  { title: "useEventCallback", group: "B" },
+  { title: "useOnlineStatus", group: "B" },
+  { title: "useClipboard", group: "B" },
+  { title: "useCountdown", group: "B" },
+] as const;
 
-const hooksB: Hook<number, string>[] = [
-  {
-    id: 1,
-    title: "useDebounceCallback",
-  },
-  {
-    id: 2,
-    title: "useEventListener",
-  },
-  {
-    id: 3,
-    title: "useEventCallback",
-  },
-  {
-    id: 4,
-    title: "useOnlineStatus",
-  },
-  {
-    id: 5,
-    title: "useClipboard",
-  },
-  {
-    id: 6,
-    title: "useCountdown",
-  },
-];
+const renderHooks = (group: string) => {
+  return hooks
+    .filter((item) => item.group === group)
+    .map((item) => (
+      <div
+        key={item.title}
+        className={cn(
+          "group flex h-10 select-none items-center justify-center rounded-md border border-neutral-400 bg-neutral-200/50 px-20 transition-colors hover:border-neutral-500 hover:bg-neutral-200/80 dark:border-neutral-800 dark:bg-neutral-900/50 hover:dark:border-neutral-700 hover:dark:bg-neutral-900/80",
+          item.class,
+        )}
+      >
+        <span className="text-sm text-neutral-600 dark:text-neutral-400">
+          {item.title}
+        </span>
+      </div>
+    ));
+};
 
 const cards: Card[] = [
   {
@@ -101,37 +80,13 @@ const cards: Card[] = [
       <>
         <div className="flex h-full items-center justify-center">
           <div className="grid grid-cols-1 gap-2 xl:grid-cols-2">
-            {hooks.map((item) => (
-              <div
-                key={item.id}
-                className={cn(
-                  "group flex h-10 select-none items-center justify-center rounded-md border border-neutral-400 bg-neutral-200/50 px-20 transition-colors hover:border-neutral-500 hover:bg-neutral-200/80 dark:border-neutral-800 dark:bg-neutral-900/50 hover:dark:border-neutral-700 hover:dark:bg-neutral-900/80",
-                  item.class,
-                )}
-              >
-                <span className="text-sm text-neutral-600 dark:text-neutral-400">
-                  {item.title}
-                </span>
-              </div>
-            ))}
+            {renderHooks("A")}
           </div>
         </div>
         <hr className="my-6 w-full rounded-full border-t-4 border-t-neutral-300 dark:border-t-neutral-900" />
         <div className="hidden items-center justify-center xl:flex">
           <div className="grid grid-cols-1 gap-2 xl:grid-cols-2">
-            {hooksB.map((item) => (
-              <div
-                key={item.id}
-                className={cn(
-                  "group flex h-10 select-none items-center justify-center rounded-md border border-neutral-400 bg-neutral-200/50 px-20 transition-colors hover:border-neutral-500 hover:bg-neutral-200/80 dark:border-neutral-800 dark:bg-neutral-900/50 hover:dark:border-neutral-700 hover:dark:bg-neutral-900/80",
-                  item.class,
-                )}
-              >
-                <span className="text-sm text-neutral-600 dark:text-neutral-400">
-                  {item.title}
-                </span>
-              </div>
-            ))}
+            {renderHooks("B")}
           </div>
         </div>
       </>
@@ -170,7 +125,7 @@ const cards: Card[] = [
       "Rehooks is open-source under the MIT license, fostering community collaboration and welcoming contributions.",
     class: "max-w-md",
   },
-];
+] as const;
 
 export function Cards() {
   return (
