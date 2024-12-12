1dbf6b4f00206f48af925b8179eb79d1b4757127
pyr33x <pyrexrj@gmail.com>
2024-12-12 12:00:48 +0330

    fix: unused

--- apps/docs/components/ui/list.tsx
+++ apps/docs/components/ui/list.tsx
@@ -57,7 +57,7 @@ export function AnimatedListItem({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <motion.div {...animations} layout className="mx-auto w-full transform-gpu">
+    <motion.div {...animations} layout className="mx-auto w-full">
       {children}
     </motion.div>
   );
