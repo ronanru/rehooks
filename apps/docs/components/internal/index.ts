c6fa3709fa7e24d18edfc5c95285b7d0c30b19e5
pyr33x <pyrexrj@gmail.com>
2024-12-12 11:22:51 +0330

    fix(docs): export

--- apps/docs/components/internal/index.ts
+++ apps/docs/components/internal/index.ts
@@ -1,6 +1,6 @@
+export { Notifications } from "./notification";
 export { AnimatedBeam } from "./beam";
 export { DotPattern } from "./dot";
 export { Cards } from "./card";
 export { Hero } from "./hero";
 export { Beam } from "./demo";
-export { List } from "./notification";
