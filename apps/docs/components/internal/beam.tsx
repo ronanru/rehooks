79aa7ee2257e5c4f1a0bb341cbfb5a46b858ffe6
pyr33x <pyrexrj@gmail.com>
2024-12-14 17:58:54 +0330

    feat(docs): use internal rehooks-ts

--- apps/docs/components/internal/beam.tsx
+++ apps/docs/components/internal/beam.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { RefObject, useEffect, useId, useState } from "react";
-import { useDebounceCallback } from "@/hooks";
+import { useDebounceCallback } from "rehooks-ts";
 import { motion } from "framer-motion";
 import { cn } from "utils";
 
