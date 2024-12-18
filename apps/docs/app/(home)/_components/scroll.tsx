96b06a8598e018d620513ccf5cca7d43f6d522a3
pyr33x <pyrexrj@gmail.com>
2024-12-18 12:38:18 +0330

    fix z-index

--- apps/docs/app/(home)/_components/scroll.tsx
+++ apps/docs/app/(home)/_components/scroll.tsx
@@ -9,7 +9,7 @@ export function Scroll() {
   return (
     <button
       onClick={() => scrollTo({ top: 0, behavior: "smooth" })}
-      className="bg-background border-border group fixed bottom-4 right-4 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border"
+      className="bg-background border-border group fixed bottom-4 right-4 z-50 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border"
     >
       <ArrowUp className="size-6 transform transition duration-100 group-hover:-translate-y-0.5" />
     </button>
