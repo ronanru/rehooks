f80d2ba8541011be66aec367961cc7e4f1f2a658
pyr33x <pyrexrj@gmail.com>
2024-12-22 22:06:38 +0330

    fix border

--- apps/docs/app/(home)/_components/cards.tsx
+++ apps/docs/app/(home)/_components/cards.tsx
@@ -68,7 +68,7 @@ export const features = [
     description:
       "Crafted with SOLID principles, ensuring type-safety and maintainability throughout the codebase.",
     className:
-      "lg:col-start-2 lg:col-end-4 lg:row-start-1 lg:row-end-1 rounded-b-2xl lg:rounded-br-none lg:rounded-tr-2xl lg:rounded-bl-none border-b-[0.5px] border-r-[1px] border-t-[1px] border-l-[1px] lg:border-l-[0.5px]",
+      "lg:col-start-2 lg:col-end-4 lg:row-start-1 lg:row-end-1 rounded-b-2xl lg:rounded-br-none lg:rounded-tr-2xl lg:rounded-bl-none border-b-[1px] lg:border-b-[0.5px] border-r-[1px] border-t-[1px] border-l-[1px] lg:border-l-[0.5px]",
     background: (
       <Retro
         angle={20}
